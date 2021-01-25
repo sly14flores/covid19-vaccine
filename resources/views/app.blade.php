8db6feca45b077c94e296aa3c475427bd727497f
Sly Flores <sly@christian.com.ph>
2021-01-25 11:52:34 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -252,8 +252,8 @@
     <script type="text/javascript" src="{{ env('APP_URL') }}/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="{{ env('APP_URL') }}/js/animate-kontext.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-	<script type="text/javascript" src="{{ env('APP_URL') }}/js/tabs.js?id={{ ran() }}"></script>
-    <script type="text/javascript" src="{{ env('APP_URL') }}/js/app.js?id={{ ran() }}"></script>
+	<script type="text/javascript" src="{{ env('APP_URL') }}/js/tabs.js?id={{ rand() }}"></script>
+    <script type="text/javascript" src="{{ env('APP_URL') }}/js/app.js?id={{ rand() }}"></script>
 
 </body>
 </html>
