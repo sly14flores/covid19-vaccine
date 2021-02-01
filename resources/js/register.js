5ce234687fa76f411fafd9fee4b352447e339b59
Sly Flores <sly@christian.com.ph>
2021-02-01 13:57:00 +0800

    Gitignore issue fixed

--- resources/js/register.js
+++
@@ -1,5 +0,0 @@
-const app = angular.module('register', []);
-
-app.controller('registerCtrl', function($scope,$http) {
-	
-});
--- resources/js/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'vuex'
-import users from './stores/users.js'
-
-export default createStore({
-    modules: {
-        users,
-    }
-})
