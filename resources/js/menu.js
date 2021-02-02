14e5b04c993318fb0e3ddd674ba0969638cbce8e
Sly Flores <sly@christian.com.ph>
2021-02-02 17:08:21 +0800

    Validations done

---
+++ resources/js/menu.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        label:'Summary',
+        icon:'pi pi-fw pi-users',
+        items:[
+          {
+              label:'Survey',
+              icon:'pi pi-th-large',
+              to: '/summary/surveys'                       
+          },              
+          {
+              label:'Registrations',
+              icon:'pi pi-fw pi-users',
+              to: '/summary/registrations'                
+          },
+        ]
+    },
+    {
+        label:'Users',
+        icon:'pi pi-fw pi-users',
+        items:[
+          {
+              label:'List',
+              icon:'pi pi-fw pi-users',
+              to: '/users'                       
+          },              
+          {
+              label:'New',
+              icon:'pi pi-fw pi-user-plus',
+              to: '/users/new'                  
+          },
+        ]
+    }
+]
