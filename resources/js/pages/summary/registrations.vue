0c502278fbdf4f8cc323b9b126436d4957886f8d
Jp <johnpaulbalanon6@gmail.com>
2021-02-18 11:01:57 +0800

    File upload

--- resources/js/pages/summary/registrations.vue
+++ resources/js/pages/summary/registrations.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
-        <MyBreadcrumb :home="home" :items="items" />        
+        <MyBreadcrumb :home="home" :items="items" />  
+         <FileUpload name="demo[]" url="./upload.php" @upload="onUpload" :multiple="true" accept="image/*" :maxFileSize="1000000">
+            <template #empty>
+                <p>Drag and drop files to here to upload.</p>
+            </template>
+        </FileUpload>
         <div class="p-grid">
             <div class="p-col-12 p-mt-2">
-                <div class="card p-fluid">
-                    <h5>List</h5>
+                <div class="card">
+                    <div class="p-grid p-col-12">
+                        <div class="p-sm-12 p-md-9 p-lg-10">
+                            <h5 class="summary p-ml-2">List</h5>
+                        </div>
+                        <div class="p-sm-12 p-md-3 p-lg-2">
+                            <button  type="button" class="p-mr-2 p-mb-2 p-button p-component p-button-icon-only" @click="refresh">
+                                <i class="pi pi-refresh icon-size"></i>
+                            </button>
+                            <button  type="button" class="p-mr-2 p-mb-2 p-button p-component p-button-success" @click="exportToExcel">
+                                    Export to Excel
+                            </button>   
+                        </div>
+                    </div>
                     <hr />
                     <DataTable :value="users">
                         <Column field="name" header="Name"></Column>
@@ -22,6 +39,7 @@
 import MyBreadcrumb from '../../components/MyBreadcrumb.vue';
 import DataTable from 'primevue/datatable/sfc';
 import Column from 'primevue/column/sfc';
+import FileUpload from 'primevue/fileupload/sfc';
 
 import store from '../../store.js'
 
@@ -36,7 +54,8 @@ export default {
     components: {
         MyBreadcrumb,
         DataTable,
-        Column
+        Column,
+        FileUpload
     },
     data() {
         return {
@@ -59,5 +78,7 @@ export default {
 </script>
 
 <style scoped>
-
+.icon-size{
+    height: 18px;
+}
 </style>
