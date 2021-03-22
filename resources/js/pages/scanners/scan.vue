74875e298358e67a6aa5259722a6e1fd1aa60f23
Sly Flores <sly@christian.com.ph>
2021-03-22 10:18:18 +0800

    Update scan.vue

--- resources/js/pages/scanners/scan.vue
+++ resources/js/pages/scanners/scan.vue
@@ -18,7 +18,7 @@
                                 <div class="p-grid">
                                     <div class="p-field p-col-12 p-md-12">
                                         <div class="center stream">
-                                            <qr-stream @decode="onDecode" class="mb p-shadow-3">
+                                            <qr-stream :camera="camera" @decode="onDecode" class="mb p-shadow-3">
                                                 <div style="color: #fe664f;" class="frame"></div>
                                             </qr-stream>
                                         </div>
@@ -392,7 +392,9 @@ import { useStore } from 'vuex'
 
 export default {
     data() {
-      
+      return {
+          camera: 'auto'
+      }
     },
     components: {
         Button,
@@ -412,16 +414,46 @@ export default {
 
         const store = useStore()
 
-        function onDecode(data) {
+        async function onInit() {
+
+        }
+
+    },
+    methods: {
+        async onInit (promise) {
+            try {
+                await promise
+            } catch (e) {
+                console.error(e)
+            } finally {
+            }
+        },        
+        async onDecode(data) {
             const str = data.split('r')
             const qr = str[1]
-            store.dispatch('vaccines/GET_BY_QR',{ id: qr })
-        }
-
-        return {
-            onDecode
-        }
-
+            this.$store.dispatch('vaccines/GET_BY_QR',{ id: qr })
+            // reset
+            // this.pause()
+            // await this.timeout(500)
+            // this.unpause()
+            //
+        },
+        async reset() {
+            this.pause()
+            await this.timeout(500)
+            this.unpause()
+        },
+        unpause () {
+            this.camera = 'auto'
+        },
+        pause () {
+            this.camera = 'off'
+        },
+        timeout (ms) {
+            return new Promise(resolve => {
+                window.setTimeout(resolve, ms)
+            })
+        }        
     }
 }
 </script>
