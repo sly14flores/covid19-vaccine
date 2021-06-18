bbe34ed60a8072a161c0d8a8cd23707db0af23cc
Sly Flores <sly@christian.com.ph>
2021-06-18 14:17:21 +0800

    Update screening.vue

--- resources/js/pages/vaccines/screening.vue
+++ resources/js/pages/vaccines/screening.vue
@@ -223,6 +223,8 @@ import { useRoute } from 'vue-router'
 import useValidate, { useVuelidate } from '@vuelidate/core'
 import { required, requiredIf } from '@vuelidate/validators'
 
+import Swal from 'sweetalert2'
+
 import {
     getPersonalInfo,
     postScreeningInfo,
