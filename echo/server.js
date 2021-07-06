e59a553a5c6bc4f95c9136a8f4da48e9607b7d53
Sly Flores <sly@christian.com.ph>
2021-07-06 08:53:26 +0800

    Set up echo server

---
+++ echo/server.js
@@ -0,0 +1,38 @@
+const {
+	devMode = true,
+	authHost = null,
+	authEndpoint = '/api/broadcasting/auth',
+	protocol = 'https',
+	port = 6002,
+	database = 'redis',
+	redisHost = 'localhost',
+	redisPort = 6379,
+	redisPassword = null,
+} = process.env;
+
+require('laravel-echo-server').run({
+    authHost: process.env.APP_URL,
+    authEndpoint,
+	protocol,
+	host: null,
+	port,
+    devMode,
+    database,
+    databaseConfig: {
+        redis: {
+            host: redisHost,
+            port: redisPort,
+			password: redisPassword
+        }
+    },
+	secureOptions: 67108864,
+	sslCertPath: "/home/misd/cert/052021/CEPO210513152748.cer",
+	sslKeyPath: "/home/misd/cert/052021/launion.key",
+	sslCertChainPath: "/home/misd/cert/052021/launion.csr",
+	apiOriginAllow: {
+		allowCors: true,
+		allowOrigin: "*",
+		allowMethods: "GET, POST",
+		allowHeaders: "Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization, X-CSRF-TOKEN, X-Socket-Id"
+	}
+});
