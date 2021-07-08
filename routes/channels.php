964718502d795d66d6f8bf124cb52794ac5940f5
Sly Flores <sly@christian.com.ph>
2021-07-08 21:45:34 +0800

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,7 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-// Broadcast::channel('vaccines.import.inoculation.{id}', function ($user, $id) {
-//     return (int) $user->id === (int) $id;
-// });
+Broadcast::channel('vaccines.import.inoculation.{id}', function ($user, $id) {
+    // return (int) $user->id === (int) $id;
+    return true;
+});
